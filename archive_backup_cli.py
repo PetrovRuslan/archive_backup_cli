ba860c6214b149feaa3bf669b47a7f331624b0dd
Timur Batyrshin <erthad@gmail.com>
2020-09-18 22:40:46 +0200

    v1

--- archive_backup_cli.py
+++ archive_backup_cli.py
@@ -2,12 +2,14 @@ import os, shutil, sys, argparse, csv, datetime
 
 source = ''
 target = ''
+compress = ''
 date = datetime.date.today()
 time = datetime.datetime.today().strftime("%H:%M:%S")
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-s', required=True, action='store', dest='source', help="Обязательный аргумент. Директория которую требуется заархивировать")
 arg_parser.add_argument('-t', required=True, action='store', dest='target', help="Обязательный аргумент. Директория в которую надо скопировать бэкап")
+arg_parser.add_argument('-c', action='store', dest='compress', default='gztar', help="Опционально. Алгоритм сжатия. По умолчанию gztar" )
 
 args = arg_parser.parse_args()
 
@@ -15,16 +17,16 @@ if os.path.exists(args.source):
     parents_dir = os.path.dirname(os.path.abspath(args.source))
     archive_dir = os.path.basename(os.path.abspath(args.source))
     if os.path.exists(args.target):
-        shutil.make_archive(f'{args.target}{archive_dir}_{date}_{time}', "gztar", parents_dir, archive_dir)
+        shutil.make_archive(f'{args.target}{archive_dir}_{date}_{time}', args.compress, parents_dir, archive_dir)
         with open('journal.csv', mode='a') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow({f'{args.source};{args.target}{archive_dir}_{date}_{time}.tar.gz;{date};{time};success'})
+            writer.writerow({f'{args.source};{args.target}{archive_dir}_{date}_{time}.{compress};{date};{time};success'})
     else:
-        os.mkdir(args.target)
-        shutil.make_archive(f'{args.target}{archive_dir}_{date}_{time}', "gztar", parents_dir, archive_dir)
+        os.makedirs(args.target)
+        shutil.make_archive(f'{args.target}{archive_dir}_{date}_{time}', args.compress, parents_dir, archive_dir)
         with open('journal.csv', mode='a') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow({f'{args.source};{args.target}{archive_dir}_{date}_{time}.tar.gz;{date};{time};success'})
+            writer.writerow({f'{args.source};{args.target}{archive_dir}_{date}_{time}.{compress};{date};{time};success'})
     print(f'{args.target}{archive_dir}_{date}_{time}.tar.gz')
 else:
     print('Исходной директории не существует')
